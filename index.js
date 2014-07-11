79d1f1501c7b764f1953c73ca4a6d99bc79a6f67
RobinQu <robinqu@gmail.com>
2014-07-11 19:36:10 +0800

    add a notice

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== "test") {
+  console.warn("testkit is not intended to be used in any situations other than test environment");
+}
+
+
 module.exports = {
   
   web: require("./lib/web")
